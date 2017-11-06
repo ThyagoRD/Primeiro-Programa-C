6cc35089438bcca12c1da59231664efd25c0b95b
201711140014 <201711140014@LABCOMP2-PC18.facomp.ufpa.com>
2017-11-06 19:53:50 -0300

    Título do commit
    Descrição das modificações feitas

---
+++ PrimeiroProgramaC.cpp
@@ -0,0 +1,10 @@
+#include "PrimeiroProgramaC.h"
+
+PrimeiroProgramaC::PrimeiroProgramaC()
+{
+}
+
+PrimeiroProgramaC::~PrimeiroProgramaC()
+{
+}
+
---
+++ PrimeiroProgramaC.h
@@ -0,0 +1,12 @@
+#ifndef PRIMEIROPROGRAMAC_H
+#define PRIMEIROPROGRAMAC_H
+
+class PrimeiroProgramaC
+{
+public:
+    PrimeiroProgramaC();
+    ~PrimeiroProgramaC();
+
+};
+
+#endif // PRIMEIROPROGRAMAC_H
